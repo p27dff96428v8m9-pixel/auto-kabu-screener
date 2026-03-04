b4cc22a58f5d563324de51e0940ab01a23bcba49
p27dff96428v8m9-pixel <p27dff96428v8m9@gmail.com>
2026-03-04 15:12:37 +0900

    Full backup updates for register_keys.py

--- register_keys.py
+++ register_keys.py
@@ -1,10 +1,15 @@
 import os
 import requests
 import base64
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 from nacl import encoding, public
 
 # GitHubリポジトリの設定
-TOKEN = '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'
+TOKEN = os.environ.get('GITHUB_TOKEN')
 REPO_NAME = 'auto-kabu-screener'
 GITHUB_API = 'https://api.github.com'
 HEADERS = {
